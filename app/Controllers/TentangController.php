41ff162dc9e84f4f246f1a2efd9448c6b7de4925
gaisanxn <bayuvalent17@gmail.com>
2024-10-25 15:54:00 +0700

    jumat25

---
+++ app/Controllers/TentangController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\TentangModel;
+
+class TentangController extends BaseController
+{
+    public function index()
+    {
+        // Inisialisasi model
+        $tentangModel = new TentangModel();
+        
+        // Ambil semua data, data akan dikembalikan sebagai object
+        $data['tentang'] = $tentangModel->first();
+        
+        // Kirim data ke view
+        return view('Beranda', $data);
+    }
+}
