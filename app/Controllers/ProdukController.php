7d3f3589120e30806892d1515940b47d3a0e7cef
gaisanxn <bayuvalent17@gmail.com>
2024-10-30 15:27:47 +0700

    rabu30

---
+++ app/Controllers/ProdukController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\ProdukModel;
+
+class ProdukController extends BaseController
+{
+    public function index()
+    {
+        // Inisialisasi model
+        $produkModel = new ProdukModel();
+        
+        // Ambil semua data, data akan dikembalikan sebagai object
+        $data['produk'] = $produkModel->first();
+        
+        // Kirim data ke view
+        return view('Artikel', $data);
+    }
+}
